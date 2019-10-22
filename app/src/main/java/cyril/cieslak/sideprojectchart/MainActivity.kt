8204c61acc10fd8416a967ff5d9b62781745f0da
Cyril442 <cyril.cieslak@gmail.com>
2019-10-22 16:25:32 +0200

    color of the line changed

--- app/src/main/java/cyril/cieslak/sideprojectchart/MainActivity.kt
+++ app/src/main/java/cyril/cieslak/sideprojectchart/MainActivity.kt
@@ -1,9 +1,11 @@
 package cyril.cieslak.sideprojectchart
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.Description
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
@@ -11,10 +13,11 @@ import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var mpLineChart : LineChart
-    lateinit var lineDataSet1 : LineDataSet
-    lateinit var dataSets : ArrayList<ILineDataSet>
-  //  lateinit var data : LineData
+    lateinit var mpLineChart: LineChart
+    lateinit var lineDataSet1: LineDataSet
+    lateinit var lineDataSet2 : LineDataSet
+    lateinit var dataSets: ArrayList<ILineDataSet>
+    lateinit var description : Description
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,25 +25,32 @@ class MainActivity : AppCompatActivity() {
 
         mpLineChart = findViewById(R.id.line_chart)
         lineDataSet1 = LineDataSet(dataValues1(), "Data Set 1")
+        lineDataSet2 = LineDataSet(dataValues2(), "Data Set 2")
         dataSets = ArrayList()
         dataSets.add(lineDataSet1)
+        dataSets.add(lineDataSet2)
 
-        Log.i("banga", " dataSets: $dataSets")
+        mpLineChart.setNoDataText("NoDatas")
+        mpLineChart.setNoDataTextColor(Color.BLUE)
+
+        mpLineChart.setDrawGridBackground(true)
+        mpLineChart.setDrawBorders(true)
+
+        lineDataSet1.setCircleColors(Color.GREEN)
+        lineDataSet1.setColor(Color.GREEN, 100)
+        lineDataSet1.lineWidth = 2F
+        lineDataSet1.valueTextSize = 10F
+        lineDataSet1.valueTextColor = Color.GREEN
+
+        description = Description()
+        description.text = "Bingo"
 
 
         var data = LineData(dataSets)
-
-        Log.i("banga", " data: $data")
-
         var getDat = data
 
-        mpLineChart.setData(getDat)
+        mpLineChart.data = getDat
         mpLineChart.invalidate()
-
-//        LineData data = new LineData(dataSets);
-//        mpLineChart.setData(data);
-//        mpLineChart.invalidate()
-
 
     }
 
@@ -57,16 +67,16 @@ class MainActivity : AppCompatActivity() {
         return dataVals
     }
 
-//    private ArrayList<Entry> dataValues1 () {
-//
-//        ArrayList<Entry> dataVals = new ArrayList <Entry> ();
-//        dataVals.add(new Entry(0, 20));
-//        dataVals.add(new Entry(1, 42));
-//        dataVals.add(new Entry(2, 15));
-//        dataVals.add(new Entry(3, 3));
-//        dataVals.add(new Entry(4, 38));
-//
-//        return dataVals;
-//
-//    }
+    private fun dataValues2(): ArrayList<Entry> {
+        val dataVals = ArrayList<Entry>()
+        dataVals.add(Entry(0F, 12F))
+        dataVals.add(Entry(1F, 18F))
+        dataVals.add(Entry(2F, 24F))
+        dataVals.add(Entry(3F, 44F))
+        dataVals.add(Entry(4F, 12F))
+
+        Log.i("banga", " dataVals: $dataVals")
+
+        return dataVals
+    }
 }
