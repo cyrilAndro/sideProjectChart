245f63556463dab23e7775e81dbab67e06969fac
Cyril442 <cyril.cieslak@gmail.com>
2019-11-12 17:17:09 +0100

    Third Activity created

--- app/src/main/java/cyril/cieslak/sideprojectchart/ThirdActivity.kt
+++ app/src/main/java/cyril/cieslak/sideprojectchart/ThirdActivity.kt
@@ -19,203 +19,34 @@ import com.github.mikephil.charting.utils.ViewPortHandler
 import kotlinx.android.synthetic.main.activity_grouped_stacked.*
 import java.text.DecimalFormat
 
-class GroupedStackedActivity : AppCompatActivity() {
+class ThirdActivity : AppCompatActivity() {
 
     private var chart: CombinedChart? = null
     private val count = 12
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_grouped_stacked)
+        setContentView(R.layout.activity_third)
 
         // Change Activity Button Listener
         button_change_activity.setOnClickListener {
 
-            val intent = Intent(this, ThirdActivity::class.java)
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+
         }
 
-        barsOnly()
         combinedFingerCrossed()
 
     }
 
-    fun barsOnly  (){
 
-        val barWidth: Float
-        val barSpace: Float
-        val groupSpace: Float
-        val groupCount = 12
-
-        barWidth = 0.15f
-        barSpace = 0.07f
-        groupSpace = 0.56f
-
-        // 1. Enter the chart into the scope
-        var barChartView = findViewById<BarChart>(R.id.chartConsumptionGraph)
-
-
-        // 2. Define the X axis values
-        var xAxisValues = ArrayList<String>()
-        xAxisValues.add("Jan")
-        xAxisValues.add("Feb")
-        xAxisValues.add("Mar")
-        xAxisValues.add("Apr")
-        xAxisValues.add("May")
-        xAxisValues.add("June")
-        xAxisValues.add("Jul")
-        xAxisValues.add("Aug")
-        xAxisValues.add("Sep")
-        xAxisValues.add("Oct")
-        xAxisValues.add("Nov")
-        xAxisValues.add("Dec")
-
-
-        // 3. Define Y axis and Stacked Datas
-
-        var yValueGroup1 = ArrayList<BarEntry>()
-        var yValueGroup2 = ArrayList<BarEntry>()
-
-
-        yValueGroup1.add(BarEntry(1f, floatArrayOf(9.toFloat(), 3.toFloat())))
-        yValueGroup2.add(BarEntry(1f, floatArrayOf(2.toFloat(), 7.toFloat())))
-
-
-        yValueGroup1.add(BarEntry(2f, floatArrayOf(3.toFloat(), 3.toFloat())))
-        yValueGroup2.add(BarEntry(2f, floatArrayOf(4.toFloat(), 15.toFloat())))
-
-        yValueGroup1.add(BarEntry(3f, floatArrayOf(3.toFloat(), 3.toFloat())))
-        yValueGroup2.add(BarEntry(3f, floatArrayOf(4.toFloat(), 15.toFloat())))
-
-        yValueGroup1.add(BarEntry(4f, floatArrayOf(3.toFloat(), 3.toFloat())))
-        yValueGroup2.add(BarEntry(4f, floatArrayOf(4.toFloat(), 15.toFloat())))
-
-
-        yValueGroup1.add(BarEntry(5f, floatArrayOf(9.toFloat(), 3.toFloat())))
-        yValueGroup2.add(BarEntry(5f, floatArrayOf(10.toFloat(), 6.toFloat())))
-
-        yValueGroup1.add(BarEntry(6f, floatArrayOf(11.toFloat(), 1.toFloat())))
-        yValueGroup2.add(BarEntry(6f, floatArrayOf(12.toFloat(), 2.toFloat())))
-
-
-        yValueGroup1.add(BarEntry(7f, floatArrayOf(11.toFloat(), 7.toFloat())))
-        yValueGroup2.add(BarEntry(7f, floatArrayOf(12.toFloat(), 12.toFloat())))
-
-
-        yValueGroup1.add(BarEntry(8f, floatArrayOf(11.toFloat(), 9.toFloat())))
-        yValueGroup2.add(BarEntry(8f, floatArrayOf(12.toFloat(), 8.toFloat())))
-
-
-        yValueGroup1.add(BarEntry(9f, floatArrayOf(11.toFloat(), 13.toFloat())))
-        yValueGroup2.add(BarEntry(9f, floatArrayOf(12.toFloat(), 12.toFloat())))
-
-        yValueGroup1.add(BarEntry(10f, floatArrayOf(11.toFloat(), 2.toFloat())))
-        yValueGroup2.add(BarEntry(10f, floatArrayOf(12.toFloat(), 7.toFloat())))
-
-        yValueGroup1.add(BarEntry(11f, floatArrayOf(11.toFloat(), 6.toFloat())))
-        yValueGroup2.add(BarEntry(11f, floatArrayOf(12.toFloat(), 5.toFloat())))
-
-        yValueGroup1.add(BarEntry(12f, floatArrayOf(11.toFloat(), 2.toFloat())))
-        yValueGroup2.add(BarEntry(12f, floatArrayOf(12.toFloat(), 3.toFloat())))
-
-
-        // 4. Group the Datas
-
-        var barDataSet1: BarDataSet
-        var barDataSet2: BarDataSet
-
-
-        barDataSet1 = BarDataSet(yValueGroup1, "")
-        barDataSet1.setColors(Color.BLUE, Color.RED)
-
-        barDataSet1.setDrawIcons(false)
-        barDataSet1.setDrawValues(false)
-
-        barDataSet2 = BarDataSet(yValueGroup2, "")
-        barDataSet2.setColors(Color.YELLOW, Color.RED)
-        barDataSet2.setDrawIcons(false)
-        barDataSet2.setDrawValues(false)
-
-
-        // Pass Both Bar Data Set's in Bar Data.
-        var barData = BarData(barDataSet1, barDataSet2)
-
-        barChartView.description.isEnabled = false
-        barChartView.description.textSize = 0f
-        barData.setValueFormatter(LargeValueFormatter())
-        barChartView.setData(barData)
-        barChartView.getBarData().setBarWidth(barWidth)
-        barChartView.getXAxis().setAxisMinimum(0f)
-        barChartView.getXAxis().setAxisMaximum(12f)
-        barChartView.groupBars(0f, groupSpace, barSpace)
-        //   barChartView.setFitBars(true)
-        barChartView.getData().setHighlightEnabled(false)
-        barChartView.invalidate()
-
-
-
-        // 5. Customize Bar Chart Legends using Legend Entry
-
-        // set bar label
-        var legend = barChartView.legend
-        legend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM)
-        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT)
-        legend.setOrientation(Legend.LegendOrientation.HORIZONTAL)
-        legend.setDrawInside(false)
-
-        var legenedEntries = arrayListOf<LegendEntry>()
-
-        legenedEntries.add(LegendEntry("2016", Legend.LegendForm.SQUARE, 8f, 8f, null, Color.RED))
-        legenedEntries.add(LegendEntry("2017", Legend.LegendForm.SQUARE, 8f, 8f, null, Color.YELLOW))
-
-        legend.setCustom(legenedEntries)
-
-        legend.setYOffset(2f)
-        legend.setXOffset(2f)
-        legend.setYEntrySpace(0f)
-        legend.setTextSize(5f)
-
-
-        // 6. Populate X Axis Datas
-
-        val xAxis = barChartView.getXAxis()
-        xAxis.setGranularity(1f)
-        xAxis.setGranularityEnabled(true)
-        xAxis.setCenterAxisLabels(true)
-        xAxis.setDrawGridLines(true)
-        xAxis.textSize = 9f
-
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM)
-        xAxis.setValueFormatter(IndexAxisValueFormatter(xAxisValues))
-
-        xAxis.setLabelCount(12)
-        xAxis.mAxisMaximum = 12f
-        xAxis.setCenterAxisLabels(true)
-        xAxis.setAvoidFirstLastClipping(true)
-        xAxis.spaceMin = 4f
-        xAxis.spaceMax = 4f
-
-
-        // 7. Modify Y Axis
-
-        val leftAxis = barChartView.getAxisLeft()
-        leftAxis.setValueFormatter(LargeValueFormatter())
-        leftAxis.setDrawGridLines(false)
-        leftAxis.setSpaceTop(1f)
-        leftAxis.setAxisMinimum(0f)
-
-        barChartView.data = barData
-        barChartView.setVisibleXRange(1f, 12f)
-
-
-    }
 
     fun combinedFingerCrossed (){
 
         title = "Weather Forecast Today"
 
-        chart = findViewById(R.id.chartCombined)
+        chart = findViewById(R.id.chartCombinedTest)
         chart!!.description.isEnabled = false
         chart!!.setBackgroundColor(Color.WHITE)
         chart!!.setDrawGridBackground(false)
@@ -307,7 +138,7 @@ class GroupedStackedActivity : AppCompatActivity() {
         set1.lineWidth = 2.5f
 //        set1.setCircleColor(Color.rgb(240, 238, 70))
 //        set1.circleRadius = 5f
-      //  set1.fillColor = Color.rgb(240, 238, 70)
+        //  set1.fillColor = Color.rgb(240, 238, 70)
         set1.mode = LineDataSet.Mode.LINEAR
         set1.setDrawValues(false)
 //        set1.valueTextSize = 10f
@@ -317,7 +148,7 @@ class GroupedStackedActivity : AppCompatActivity() {
 
         set1.axisDependency = YAxis.AxisDependency.LEFT
 
-            d.addDataSet(set1)
+        d.addDataSet(set1)
 
 
         return d
@@ -338,7 +169,7 @@ class GroupedStackedActivity : AppCompatActivity() {
 
         val set1 = BarDataSet(entries1, "Temperature °C")
         set1.color = Color.rgb(66, 165, 245)
-      // set1.color = Color.rgb(179, 229, 252)
+        // set1.color = Color.rgb(179, 229, 252)
         set1.valueTextColor = Color.WHITE
         set1.valueTextSize = 10f
         set1.axisDependency = YAxis.AxisDependency.LEFT
@@ -428,12 +259,12 @@ class GroupedStackedActivity : AppCompatActivity() {
         var biggestFigure = 10f
         var valueBiggestPlusThree = biggestFigure.plus(3f)
 
-            entries.add(BubbleEntry(0F, valueBiggestPlusThree, zero))
-            entries.add(BubbleEntry(1F, valueBiggestPlusThree, first))
-            entries.add(BubbleEntry(2F, valueBiggestPlusThree, second))
-            entries.add(BubbleEntry(3F, valueBiggestPlusThree, third))
-            entries.add(BubbleEntry(4F, valueBiggestPlusThree, fourfth))
-            entries.add(BubbleEntry(5F, valueBiggestPlusThree, fifth))
+        entries.add(BubbleEntry(0F, valueBiggestPlusThree, zero))
+        entries.add(BubbleEntry(1F, valueBiggestPlusThree, first))
+        entries.add(BubbleEntry(2F, valueBiggestPlusThree, second))
+        entries.add(BubbleEntry(3F, valueBiggestPlusThree, third))
+        entries.add(BubbleEntry(4F, valueBiggestPlusThree, fourfth))
+        entries.add(BubbleEntry(5F, valueBiggestPlusThree, fifth))
         entries.add(BubbleEntry(6F, valueBiggestPlusThree, sixth))
         entries.add(BubbleEntry(7F, valueBiggestPlusThree, seventh))
 
@@ -450,7 +281,7 @@ class GroupedStackedActivity : AppCompatActivity() {
         return bd
     }
 
-     class myValueTemperatureFormatter (): IValueFormatter {
+    class myValueTemperatureFormatter (): IValueFormatter {
         override fun getFormattedValue(
             value: Float,
             entry: Entry?,
@@ -459,18 +290,18 @@ class GroupedStackedActivity : AppCompatActivity() {
         ): String {
 
             var valueToInt = value.toInt()
-           return "$valueToInt + °C"
+            return "$valueToInt + °C"
         }
     }
 
-    class MyTempValueFormatter:IValueFormatter {
+    class MyTempValueFormatter: IValueFormatter {
 
         private val mFormat: DecimalFormat
         init{
             mFormat = DecimalFormat("###,###,##0.0") // use one decimal
         }
 
-        override fun getFormattedValue(value:Float, entry:Entry, dataSetIndex:Int, viewPortHandler:ViewPortHandler):String {
+        override fun getFormattedValue(value:Float, entry: Entry, dataSetIndex:Int, viewPortHandler: ViewPortHandler):String {
             // write your logic here
             return mFormat.format(value) + " $" // e.g. append a dollar-sign
         }
